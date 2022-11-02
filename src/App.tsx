b4e0559a3c580df123f80d0ff8f833c85f469886
denis <denis@latticeflow.ai>
2022-11-02 23:37:19 +0200

    Bring back the alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Button, Drawer, Form, InputNumber } from "antd";
+import { Alert, Button, Drawer, Form, InputNumber, Space } from "antd";
 import { useState } from "react";
 
 import { Game } from "./Game";
@@ -27,41 +27,47 @@ export function App(): JSX.Element {
         placement="right"
         onClose={() => setOpenedConfigDrawer(false)}
         open={openedConfigDrawer}
-        width={240}
+        width={256}
         destroyOnClose={true}
       >
-        <Form
-          layout="vertical"
-          className="game-settings"
-          initialValues={gameParams}
-          onFinish={(x) => {
-            setGameParams(x);
-            setOpenedConfigDrawer(false);
-          }}
-        >
-          <Form.Item label="Rows" name="rows" rules={[{ required: true }]}>
-            <InputNumber min={MIN_GRID_SIZE} max={MAX_GRID_SIZE} />
-          </Form.Item>
-          <Form.Item
-            label="Columns"
-            name="columns"
-            rules={[{ required: true }]}
+        <Space direction="vertical" size={16}>
+          <Alert
+            message="Changing the settings will reset the game state."
+            type="warning"
+          />
+          <Form
+            layout="vertical"
+            className="game-settings"
+            initialValues={gameParams}
+            onFinish={(x) => {
+              setGameParams(x);
+              setOpenedConfigDrawer(false);
+            }}
           >
-            <InputNumber min={MIN_GRID_SIZE} max={MAX_GRID_SIZE} />
-          </Form.Item>
-          <Form.Item
-            label="Obstacles"
-            name="obstacles"
-            rules={[{ required: true }]}
-          >
-            <InputNumber min={0} max={4} />
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit">
-              Save Changes
-            </Button>
-          </Form.Item>
-        </Form>
+            <Form.Item label="Rows" name="rows" rules={[{ required: true }]}>
+              <InputNumber min={MIN_GRID_SIZE} max={MAX_GRID_SIZE} />
+            </Form.Item>
+            <Form.Item
+              label="Columns"
+              name="columns"
+              rules={[{ required: true }]}
+            >
+              <InputNumber min={MIN_GRID_SIZE} max={MAX_GRID_SIZE} />
+            </Form.Item>
+            <Form.Item
+              label="Obstacles"
+              name="obstacles"
+              rules={[{ required: true }]}
+            >
+              <InputNumber min={0} max={4} />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Save Changes
+              </Button>
+            </Form.Item>
+          </Form>
+        </Space>
       </Drawer>
     </div>
   );
